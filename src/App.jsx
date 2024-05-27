d54ddeeb2bde1124cda4e6607129ec1b2ed51e4f
hyejin <po852alth@naver.com>
2024-05-27 17:26:06 +0900

    1st homework

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,29 @@
-function App() {
-  return <div></div>;
-}
+import React from "react";
+import { useState } from "react";
+import "./App.css";
+
+export const App = () => {
+  const [count, setCount] = useState(0);
+
+  const addCount = () => {
+    setCount(count + 1);
+  };
+  const minusCount = () => {
+    setCount(count - 1);
+  };
+
+  const resetCount = () => {
+    setCount(0);
+  };
+
+  return (
+    <>
+      <p>{count}</p>
+      <button onClick={addCount}> + </button>
+      <button onClick={minusCount}> - </button>
+      <button onClick={resetCount}> reset </button>
+    </>
+  );
+};
 
 export default App;
