fad7b65401f841cdd863153a4d0e26c11b7cd141
hyejin <po852alth@naver.com>
2024-05-27 17:11:54 +0900

    App.jsx 불필요한코드삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,5 @@
-import { useState } from "react";
-
 function App() {
-  const [isdark, setIsDark] = useState(false);
-
-  const changeMode = () => {
-    setIsDark(!isdark);
-  };
-
-  return (
-    <div>
-      {isdark ? "다크모드" : "화이트모드"}
-      <button onClick={changeMode}>change</button>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
